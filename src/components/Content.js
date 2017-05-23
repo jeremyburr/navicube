cd4091b74c3d266f7247bb6dc2ef967605abc9fb
jeremyburr <jeremy@psyburr.com>
2017-05-23 02:12:07 +0100

    Endeavors edit

--- src/components/Content.js
+++ src/components/Content.js
@@ -52,7 +52,7 @@ let Content = ({name, position}) => {
 					<Col xs={8}> 
 						<div style={[style.all, style[position]]}>
 								<p>
-									I am Jeremy, a front-end developer and aspring full-stack engineer. This is the humble beginning of Psyburr, a virtual space for hosting my programming endeavors. It is undergoing constant revision. The source code is on Github.
+									I'm Jeremy, a front-end developer and aspring full-stack engineer. Psyburr is a virtual space for hosting my programming endeavors. It is undergoing constant revision. The source code is on <a href="https://github.com/jeremyburr/psyburr">GitHub</a>
 </p>
 						</div>
 					</Col>
@@ -65,31 +65,16 @@ let Content = ({name, position}) => {
 					<Col xs={2} />
 					<Col xs={8}>
 						<div style={[style.all, style[position]]}>
-							<h3> Vulkern </h3>
+							<h3> Pzilogenix </h3>
 								<p>
-								Vulkern is a physical command interface system. It allows a user to operate a computer with body movement by creating a real-time kinesthetic feedback loop between the user and computer. It includes an input device suit, microcontrollers, drivers, command-line tools, GUI application, and physical command language.
-								</p> 
-								<p>
-								Input Device: A number of approaches - both wired and wireless - are being considered. Use of gyroscopes, accelerometers, and magnets will be explored, as will any applicable developments the emerging area of virtual reality suits.
-								</p> 
-							<h3> Kidelix </h3>
-								<p>
-								Kidelix is an intuitive vector-based 3D modeling framework. It integrates a set of mathematical principles, including crystal lattice systems, L-system fractals, phi, electromagnetic fields, kaleidoscopes, and cellular automatons.
+								Pzilogenix is a vector-based 3D modeling framework that draws from mathematical concepts including crystal lattices, snowflakes, L-system fractals, phi, electromagnetic fields, kaleidoscopes, and cellular automatons, as well as psychedelic art.
 							  </p>
-								<p>Its core library will start with three categories: human anatomy, plant life, and architecture.	Interactive mode allows kinesthetic control with Vulkern.
+								<p>Its core library will start with three categories: people, plant life, and architecture.
 								</p> 
-							<h3> Veagrow </h3>
+								<h3> Xelenix </h3>  
 							<p>
-								Veagrow is an automated plant growth system. Its interchangeable units of tube and wiring mappings allow for easy scaling and customization. Solar powered options will be explored. 	
-							</p>
-							<h3> Xelenix </h3>  
-							<p>
-								Xelenix is an interactive virtual space showcasing models developed with Kidelix. A world of fantasy, Xelenix is as much a feat of art as it is programing
+								Xelenix is an interactive virtual space showcasing models developed with Pzilogenix. A world of fantasy, Xelenix is as much a feat of art as it is programing.  
 						</p>
-							<h3> Psilogenix </h3>
-							<p>
-							Psilogenix is an experimental visual pattern generator developed with Kidelix. It draws influence heavily from psychedic art.
-						  </p>
 						</div>
 					</Col>
 					<Col xs={2} />
